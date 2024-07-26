fca87084aea738f248d66a226846676b3b9bf8fd
joao magnago <joaomagnago.wm@gmail.com>
2024-07-26 20:39:36 -0300

    feat: apply method to cancel an existing schedule in page

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,16 +1,23 @@
+import { scheduleCancel } from "../../services/schedule-cancel.js"
+import { schedulesDay } from "./load.js"
+
 const periods = document.querySelectorAll(".period")
 
-periods.forEach((periods) => {
-  periods.addEventListener("click", (event) => {
-    if (event.target.classlist.contains("cancel-icon")) {
+periods.forEach((period) => {
+  period.addEventListener("click", async (event) => {
+    if (event.target.classList.contains("cancel-icon")) {
       const item = event.target.closest("li")
+
       const { id } = item.dataset
+
+      console.log(id)
 
       if (id) {
         const isConfirm = confirm("Tem certeza que deseja cancelar este agenamento?")
 
         if (isConfirm) {
-          // remove logic here
+          await scheduleCancel({ id })
+          schedulesDay()
         }
       }
     }
